8cceedd5842f3c0f2646dcee7999c4ace3af63f8
jagernau <stepanenkov25@gmail.com>
2023-02-11 00:33:28 +0300

    vault backup: 2023-02-11 00:33:28

--- Code/oop/what_is_class.py
+++ Code/oop/what_is_class.py
@@ -1,9 +1,24 @@
 
 class My:
-    value = 10
+    x = 10
+    def __init__(self):
+        self.y = 777
 
     def get_value(self):
-        return self.value
+        return self.x
 
-MyClass = type("MyClass", (object, ), dict())
-print(MyClass)
+try:
+    My.y = 666 
+    # видимо при записи числа, y автоматически инициализируется
+    print(My.y)
+except BaseException:
+    print("y ещё не появился")
+finally:
+    my = My()
+    My.x = 20
+    my.y = 26666
+    print(f"{My}\n{my}\n{My.x}\n{my.y}")
+
+
+
+
