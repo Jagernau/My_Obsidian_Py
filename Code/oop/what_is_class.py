c36d04d3f8cdeda8f0d468dd2b641a5b31daa6ba
jagernau <stepanenkov25@gmail.com>
2023-02-11 13:27:16 +0300

    vault backup: 2023-02-11 13:27:16

--- Code/oop/what_is_class.py
+++ Code/oop/what_is_class.py
@@ -1,24 +1,26 @@
-
 class My:
-    x = 10
+    x = 10  # просто атрибут класса
+
     def __init__(self):
-        self.y = 777
+        self.y = 777  # атр. экз. класса
 
     def get_value(self):
         return self.x
 
+
 try:
-    My.y = 666 
+    My.y = 666
     # видимо при записи числа, y автоматически инициализируется
     print(My.y)
 except BaseException:
     print("y ещё не появился")
 finally:
-    my = My()
+    my = My()  #Создали экземпляр класса
     My.x = 20
     my.y = 26666
-    print(f"{My}\n{my}\n{My.x}\n{my.y}")
-
-
-
-
+    print(My)   #Класс это некий чертёж объекта,
+    print(id(My)) #С сылкой для идентификации
+    print(my)
+    print(id(my))
+    print(My.x)
+    print(my.y)
