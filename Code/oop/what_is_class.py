9f46dd242b15c281da3a01efbd6b70189c41e886
jagernau <stepanenkov25@gmail.com>
2023-02-11 13:32:14 +0300

    vault backup: 2023-02-11 13:32:14

--- Code/oop/what_is_class.py
+++ Code/oop/what_is_class.py
@@ -15,11 +15,11 @@ try:
 except BaseException:
     print("y ещё не появился")
 finally:
-    my = My()  #Создали экземпляр класса
+    my = My()  # Создали экземпляр класса
     My.x = 20
     my.y = 26666
-    print(My)   #Класс это некий чертёж объекта,
-    print(id(My)) #С сылкой для идентификации
+    print(My)  # Класс это некий чертёж объекта,
+    print(id(My))  # С сылкой для идентификации
     print(my)
     print(id(my))
     print(My.x)
